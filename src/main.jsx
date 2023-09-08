e7a5f61c71e1584c2a82181b30d7c631a6057809
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-09-08 11:25:21 -0500

    React Installation

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LandingPage from './pages/landingPage/LandingPage.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+    <LandingPage />
+)
