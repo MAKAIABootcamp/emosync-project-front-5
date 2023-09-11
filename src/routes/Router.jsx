6a6ac792c28969072bb7328ad2c419e59ac20f43
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-09-10 22:24:03 -0500

    WeekSchedule Feature Design

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from '../pages/landingPage/LandingPage'
 import FeedPsycho from '../pages/psychology/feed/FeedPsycho'
+import WeekSchedule from '../pages/psychology/weekSchedule/WeekSchedule'
 
 
 const Router = () => {
   return (
    <BrowserRouter>
         <Routes>
-            <Route path='/' element={<FeedPsycho/>}>
+            <Route path='/' element={<WeekSchedule/>}>
             </Route>
         </Routes>
    </BrowserRouter>
