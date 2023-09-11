2d1a2e59c98160379dd59cc55061c90035cea672
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-09-11 00:11:02 -0500

    NewSchedule Feature design

---
+++ src/pages/psychology/history/HistoryPsycho.jsx
@@ -0,0 +1,49 @@
+import "./main.scss"
+import React from 'react'
+import HeaderPsycho from '../../../components/headerPsycho/HeaderPsycho'
+
+const HistoryPsycho = () => {
+    const actualPage = 1
+    const prevPage = 1
+  return (
+    <main className='WeekSchedule__father'>
+    <HeaderPsycho />
+    <section className='HistoryPsycho'>
+        <h2>Historial de citas de Alejandra Sanchez</h2>
+        <section className='WeekSchedule__table HistoryPsycho__table'>
+            <div className='WeekSchedule__table__celda upLeft'><h3>Nombre del paciente</h3> </div>
+            <div className='WeekSchedule__table__celda   middle'><h3>Fecha de la cita</h3> </div>
+            <div className='WeekSchedule__table__celda middle'><h3>Nombre del psicologo tratante</h3> </div>
+            <div className='WeekSchedule__table__celda upRight'><h3></h3> </div>
+            <div  className='WeekSchedule__table__celda'> <span>Alejandra Sanchez</span></div>
+            <div className='WeekSchedule__table__celda'><span>7 de octubre 12:00pm</span></div>
+            <div className='WeekSchedule__table__celda'><strong>Armelio Camargo</strong></div>
+            <div className='WeekSchedule__table__celda cell_details'><figure><img src="/Psychologist/details.svg" alt="details.svg" /></figure></div>
+            <div  className='WeekSchedule__table__celda'> <span>Alejandra Sanchez</span></div>
+            <div className='WeekSchedule__table__celda'><span>7 de octubre 12:00pm</span></div>
+            <div className='WeekSchedule__table__celda'><strong>Armelio Camargo</strong></div>
+            <div className='WeekSchedule__table__celda cell_details'><figure><img src="/Psychologist/details.svg" alt="details.svg" /></figure></div>
+            <div  className='WeekSchedule__table__celda'> <span>Alejandra Sanchez</span></div>
+            <div className='WeekSchedule__table__celda'><span>7 de octubre 12:00pm</span></div>
+            <div className='WeekSchedule__table__celda'><strong>Armelio Camargo</strong></div>
+            <div className='WeekSchedule__table__celda cell_details'><figure><img src="/Psychologist/details.svg" alt="details.svg" /></figure></div>
+            <div  className='WeekSchedule__table__celda downLeft'> <span>Alejandra Sanchez</span></div>
+            <div className='WeekSchedule__table__celda '><span>7 de octubre 12:00pm</span></div>
+            <div className='WeekSchedule__table__celda'><strong>Armelio Camargo</strong></div>
+            <div className='WeekSchedule__table__celda downRight cell_details'> <figure><img src="/Psychologist/details.svg" alt="details.svg" /></figure> </div>
+        </section>
+        <section className='WeekSchedule__pager'>
+            <p className='WeekSchedule__pager_page'>
+                {`${actualPage} - ${prevPage}`}
+            </p>
+            <section className='WeekSchedule__pager__arrows'>
+            <figure><img src="/Psychologist/arrow-prev.svg" alt="arrow-prev" /></figure>
+            <figure><img src="/Psychologist/arrow-next.svg" alt="arrow-next" /></figure>
+            </section>
+        </section>
+    </section>
+</main>
+  )
+}
+
+export default HistoryPsycho
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from '../pages/landingPage/LandingPage'
 import FeedPsycho from '../pages/psychology/feed/FeedPsycho'
 import WeekSchedule from '../pages/psychology/weekSchedule/WeekSchedule'
+import HistoryPsycho from '../pages/psychology/history/HistoryPsycho'
+import NewSchedule from '../pages/psychology/newSchedule/NewSchedule'
 
 
 const Router = () => {
   return (
    <BrowserRouter>
         <Routes>
-            <Route path='/' element={<WeekSchedule/>}>
+            <Route path='/' element={<NewSchedule/>}>
             </Route>
         </Routes>
    </BrowserRouter>
