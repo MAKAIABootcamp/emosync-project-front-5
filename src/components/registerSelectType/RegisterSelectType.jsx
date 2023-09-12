53ec8ff6f4e5bf9aabab314352dacb364f8d7e75
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-09-11 21:32:32 -0500

    RegisterSelectType Feature design

--- src/components/registerSelectType/RegisterSelectType.jsx
+++ src/components/registerSelectType/RegisterSelectType.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import "./main.scss"
+
 
 const RegisterSelectType = () => {
   return (
-    <div>RegisterSelectType</div>
+    <main className='registerSelectType'>
+    <article className='registerSelectType__container'>
+      <h2 className='registerSelectType__title'>
+        Bienvenido a <span>EMOSYNC</span>
+      </h2>
+      <h1 className='registerSelectType__descrip'>Nos da gusto que hayas decidido hacer de esto. <br /> En primera instancia, cuentanos un poco de ti, Eres?</h1>
+     <section className='registerSelectType__figures'>
+     <figure className='registerSelectType__figures__user'><img src="/Register/client.png" alt="client" />
+     <h3>Paciente</h3>
+     </figure>
+     <figure className='registerSelectType__figures__psico'><img src="/Register/psychologist.png" alt="psycologist" />
+     <h3>Psicologo</h3>
+     </figure>
+     </section>
+    
+     
+     <button type='submit' className='registerSelectType__btn'>Continuar</button>
+        
+    
+    </article>
+  </main>
   )
 }
 
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from '../pages/landingPage/LandingPage'
-import Login from '../pages/login/Login'
+import Login from '../pages/login/login'
 import Welcome from '../pages/welcome/Welcome'
 import Register from '../pages/register/Register'
+import RegisterSelectType from '../components/registerSelectType/RegisterSelectType'
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LandingPage />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={<Login/>} />
         <Route path='/welcome' element={<Welcome />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/RegisterSelectType' element={<RegisterSelectType/>} />
       </Routes>
     </BrowserRouter>
   )
